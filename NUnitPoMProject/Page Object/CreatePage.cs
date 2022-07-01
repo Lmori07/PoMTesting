07f1d2516d66ace313d08959be00ecfeaab9d19b
Lmori07 <33818263+Lmori07@users.noreply.github.com>
2022-06-30 20:50:20 -0400

    Error Commit

--- NUnitPoMProject/Page Object/CreatePage.cs
+++ NUnitPoMProject/Page Object/CreatePage.cs
@@ -1,23 +1,23 @@
 namespace NUnitPoMProject;
 
-
 public class CreatePage
 {
     //Constructor que me permitira utilizar los metodos del WebDriver
-    public CreatePage(IWebDriver driver)
+    public CreatePage(IWebDriver driver, SelectElement selectDay)
     {
-        Driver = driver;   
+        Driver = driver;
     }
 
     private IWebDriver Driver{get;}
 
-    //Preparo el locator de loselemento que estoy buscando.
+    #region Locators
+    //Preparo el locator de los elemento que estoy buscando.
     public IWebElement rdoBtnGender => Driver.FindElement(By.Id("id_gender1"));
     public IWebElement txtBoxFirstName => Driver.FindElement(By.Id("customer_firstname"));
     public IWebElement txtBoxLastName => Driver.FindElement(By.Id("customer_lastname"));
     public IWebElement txtBoxEmail => Driver.FindElement(By.Id("email"));
-    public IWebElement txtBoxPassword => Driver.FindElement(By.Id("passwd"));
-    public IWebElement ddDays => Driver.FindElement(By.Id("days"));
+    public IWebElement txtBoxPassword => Driver.FindElement(By.Id("passwd")); 
+    public IWebElement ddDays => Driver.FindElement(By.Id("days")); 
     public IWebElement ddMonths => Driver.FindElement(By.Id("months"));
     public IWebElement ddYears => Driver.FindElement(By.Id("years"));
     public IWebElement chkBoxNewsletter => Driver.FindElement(By.Id("newsletter"));
@@ -36,12 +36,115 @@ public class CreatePage
     public IWebElement txtBoxMobile => Driver.FindElement(By.Id("phone_mobile"));
     public IWebElement txtBoxAddressAlias => Driver.FindElement(By.Id("alias"));
     public IWebElement btnRegister => Driver.FindElement(By.Id("submitAccount"));
+    #endregion
 
     //Ejecuto las operaciones sobre el elemeneto en cuestion.
-   // public void clickOnEmailTxtBox() => txtbEmail.Click();
-   // public void sendValidMail()=> txtbEmail.SendKeys("prueba@prueba.com");
-   // public void clickOnBtnCreate()=> btnCreate.Click();
+    #region Select Title
+    public void selectTitle() => rdoBtnGender.Click();
+    #endregion
+
+    #region First Name TextBox
+    public void clearingFirstNameTbox() => txtBoxFirstName.Clear();
+    public void writtingFirstNameTbox() => txtBoxFirstName.SendKeys("");
+    #endregion
+
+    #region Last Name TextBox
+    public void clearingLastNameTbox() => txtBoxLastName.Clear();
+    public void writtingLastNameTbox() => txtBoxLastName.SendKeys("");
+    #endregion
+
+    #region Email TextBox
+    public void clearingEmailTbox() => txtBoxEmail.Clear();
+    public void writtingEmailTbox() => txtBoxEmail.SendKeys("");
+    #endregion
+
+    #region Password TextBox
+    public void clearingPasswordTbox() => txtBoxPassword.Clear();
+    public void writtingPasswordTbox() => txtBoxPassword.SendKeys("");
+    #endregion
+
+    #region Date of Birth
+    //Generando la lista de los elementos de cada dropdowns
+    //dayElements.
+
+    SelectElement selectDay = new SelectElement(ddDays);
+    SelectElement selectMonth = new SelectElement(ddMonths);
+    SelectElement selectYear = new SelectElement()ddYears;
+    public void selectingDay() => selectDay.SelectByValue("7");
+    public void selectingMonth() => selectMonth.SelectByValue("August");
+    public void selectingYear() => selectYear.SelectByValue("1987");
+    #endregion
+
+    #region Newsletter and Special Offers
+    public void selectNewsletter() => chkBoxNewsletter.Click();
+    public void selectSpecialOffer() => chkBoxSpecialOffer.Click();
+    #endregion
+
+    #region First Name Address
+    public void clearingFirstNameTboxAddress() => txtBoxFirstNameAddress.Clear();
+    public void writtingFirstNameTboxAddress() => txtBoxFirstNameAddress.SendKeys("");
+    #endregion
+
+    #region Last Name Address
+    public void clearingLastNameTboxAddress() => txtBoxLastNameAddress.Clear();
+    public void writtingLastNameTboxAddress() => txtBoxLastNameAddress.SendKeys("");
+    #endregion
+
+    #region Company Address
+    public void clearingCompanyTboxAddress() => txtBoxCompany.Clear();
+    public void writtingCompanyTboxAddress() => txtBoxCompany.SendKeys("");
+    #endregion
+
+    #region Location Address
+    public void clearingLocationTboxAddress() => txtBoxAddress.Clear();
+    public void writtingLocationTboxAddress() => txtBoxAddress.SendKeys("");
+    #endregion
+
+    #region Location2 Address
+    public void clearingLocation2TboxAddress() => txtBoxAddress2.Clear();
+    public void writtingLocation2TboxAddress() => txtBoxAddress2.SendKeys("");
+    #endregion
+
+    #region City Address
+    public void clearingCityTboxAddress() => txtBoxCity.Clear();
+    public void writtingCityTboxAddress() => txtBoxCity.SendKeys("");
+    #endregion
+
+    #region State Address
+   //SelectElement stateElements = new SelectElement(ddStates);
+    #endregion
+
+    #region ZipCode Address
+    public void clearingZipCodeTboxAddress() => txtBoxZipCode.Clear();
+    public void writtingZipCodeTboxAddress() => txtBoxZipCode.SendKeys("");
+    #endregion
+
+    #region Country Address
     
+    #endregion
 
+    #region Additional Infromation
+    public void clearingAdditionalInformationTboxAddress() => txtBoxAdditionalInformation.Clear();
+    public void writtingAdditionalInformationTboxAddress() => txtBoxAdditionalInformation.SendKeys("");
+    #endregion
 
+    #region Phone
+    public void clearingPhoneTboxAddress() => txtBoxPhone.Clear();
+    public void writtingPhoneTboxAddress() => txtBoxPhone.SendKeys("");
+    #endregion
+
+    #region Mobile
+    public void clearingMobileTboxAddress() => txtBoxMobile.Clear();
+    public void writtingMobileTboxAddress() => txtBoxMobile.SendKeys("");
+    #endregion
+
+    #region Address Alias
+    public void clearingAddressAliasTboxAddress() => txtBoxAddressAlias.Clear();
+    public void writtingAddressAliasTboxAddress() => txtBoxAddressAlias.SendKeys("");
+    #endregion
+
+    #region Submit Button
+    public void ClickSubmit() => btnRegister.Click();
+    #endregion
 }
+
